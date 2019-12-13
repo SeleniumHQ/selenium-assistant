cd6b81cca711bed26051b67f6f563c50cf2d9f96
Diego Molina <diemol@gmail.com>
2019-12-13 21:19:04 +0100

    Initial version of the Selenium Assistant bot

--- index.js
+++ index.js
@@ -2,18 +2,59 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+const getConfig = require('probot-config')
+const needsTriagingLabel = 'needs-triaging'
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+module.exports = async app => {
+  app.log('Selenium Assistant has started!')
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on('issues.opened', triageOpenedIssues)
+
+  async function triageOpenedIssues (context) {
+    const config = await getConfig(context, 'selenium-assistant.yml')
+    if (!context.isBot && config) {
+      // The bot greets the user first
+      let comment = context.issue({ body: config.openIssueGreetingComment })
+      await context.github.issues.createComment(comment)
+      const issueBody = context.payload.issue.body.toLowerCase()
+      if (issueBody.includes('💬') || issueBody.includes('questions and help')) {
+        // It is a question, the bot closes it
+        comment = context.issue({ body: config.closeQuestionsAndSupportRequestsComment })
+        await context.github.issues.createComment(comment)
+        if (config.closeQuestionsAndSupportRequests) {
+          await context.github.issues.update({
+            owner: context.issue().owner,
+            repo: context.issue().repo,
+            issue_number: context.issue().number,
+            state: 'closed'
+          })
+        }
+      } else {
+        // The bot cannot triage it, it labels the issue
+        await ensureNeedsTriagingLabelExists(context)
+        return context.github.issues.addLabels({
+          owner: context.issue().owner,
+          repo: context.issue().repo,
+          issue_number: context.issue().number,
+          labels: [needsTriagingLabel]
+        })
+      }
+    }
+  }
+
+  async function ensureNeedsTriagingLabelExists (context) {
+    return context.github.issues.getLabel({
+      owner: context.issue().owner,
+      repo: context.issue().repo,
+      name: needsTriagingLabel
+    }).catch(() => {
+      return context.github.issues.createLabel({
+        owner: context.issue().owner,
+        repo: context.issue().repo,
+        name: needsTriagingLabel,
+        color: 'ffffff'
+      })
+    })
+  }
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,20 @@
 const nock = require('nock')
 // Requiring our app implementation
-const myProbotApp = require('..')
+const seleniumAssistant = require('..')
 const { Probot } = require('probot')
 // Requiring our fixtures
-const payload = require('./fixtures/issues.opened')
-const issueCreatedBody = { body: 'Thanks for opening this issue!' }
+const issueToBeClosed = require('./fixtures/issues.question_to_be_closed')
+const issueToBeTriaged = require('./fixtures/issues.needs_to_be_triaged')
+const config = require('./fixtures/repos.contents.config')
 const fs = require('fs')
 const path = require('path')
+const yaml = require('js-yaml')
+const apiBasePath = 'https://api.github.com'
 
-describe('My Probot app', () => {
+describe('Selenium Assistant', () => {
   let probot
   let mockCert
+  let repoConfigContent
 
   beforeAll((done) => {
     fs.readFile(path.join(__dirname, 'fixtures/mock-cert.pem'), (err, cert) => {
@@ -18,31 +22,90 @@ describe('My Probot app', () => {
       mockCert = cert
       done()
     })
+    repoConfigContent = yaml.safeLoad(Buffer.from(config.content, 'base64').toString()) || {}
   })
 
   beforeEach(() => {
     nock.disableNetConnect()
     probot = new Probot({ id: 123, cert: mockCert })
     // Load our app into probot
-    probot.load(myProbotApp)
+    probot.load(seleniumAssistant)
   })
 
-  test('creates a comment when an issue is opened', async () => {
-    // Test that we correctly return a test token
-    nock('https://api.github.com')
-      .post('/app/installations/2/access_tokens')
-      .reply(200, { token: 'test' })
+  test('closes issue when a question or a support request is created', async () => {
+    // Test that we correctly return the repo config
+    nock(apiBasePath)
+      .get('/repos/seleniumhq/testing-things/contents/.github/selenium-assistant.yml')
+      .reply(200, config)
 
-    // Test that a comment is posted
-    nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
-        expect(body).toMatchObject(issueCreatedBody)
+    // Test that a greeting comments and a closing comment are posted
+    nock(apiBasePath)
+      .post('/repos/seleniumhq/testing-things/issues/1/comments', (body) => {
+        expect(body).toMatchObject({ body: repoConfigContent.openIssueGreetingComment })
+        return true
+      })
+      .reply(200)
+
+    nock(apiBasePath)
+      .post('/repos/seleniumhq/testing-things/issues/1/comments', (body) => {
+        expect(body).toMatchObject({ body: repoConfigContent.closeQuestionsAndSupportRequestsComment })
+        return true
+      })
+      .reply(200)
+
+    // Test that the issue is closed
+    nock(apiBasePath)
+      .patch('/repos/seleniumhq/testing-things/issues/1', (body) => {
+        expect(body).toMatchObject({ state: 'closed' })
         return true
       })
       .reply(200)
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
+    await probot.receive({ name: 'issues', payload: issueToBeClosed })
+  })
+
+  test('new issues which are not questions get a greeting and a label', async () => {
+    // Test that we correctly return the repo config
+    nock(apiBasePath)
+      .get('/repos/seleniumhq/testing-things/contents/.github/selenium-assistant.yml')
+      .reply(200, config)
+
+    // Test that a greeting comments and a closing comment are posted
+    nock(apiBasePath)
+      .post('/repos/seleniumhq/testing-things/issues/1/comments', (body) => {
+        expect(body).toMatchObject({ body: repoConfigContent.openIssueGreetingComment })
+        return true
+      })
+      .reply(200)
+
+    // Test that it checks if the label exists
+    nock(apiBasePath)
+      .get('/repos/seleniumhq/testing-things/labels/needs-triaging')
+      .reply(404, {
+        message: 'Not Found',
+        documentation_url: 'https://developer.github.com/v3/issues/labels/#get-a-single-label'
+      })
+
+    // Create the label since it does not exist
+    nock(apiBasePath)
+      .post('/repos/seleniumhq/testing-things/labels', (body) => {
+        expect(body).toMatchObject({ name: 'needs-triaging', color: 'ffffff' })
+        return true
+      })
+      .reply(200)
+
+    // Add the label to the issue
+    nock(apiBasePath)
+      .post('/repos/seleniumhq/testing-things/issues/1/labels', (body) => {
+        console.log(body)
+        expect(body).toContain('needs-triaging')
+        return true
+      })
+      .reply(200)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'issues', payload: issueToBeTriaged })
   })
 
   afterEach(() => {
@@ -50,9 +113,3 @@ describe('My Probot app', () => {
     nock.enableNetConnect()
   })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
